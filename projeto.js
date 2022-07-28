8b95d5244b711cd23860d49f365122cdb9d105eb
Beatriz de Araújo Andrade <80129016+BeatrizA2@users.noreply.github.com>
2022-07-28 16:50:15 -0300

    Create projeto.js

---
+++ projeto.js
@@ -0,0 +1,10 @@
+const [funcName, setfuncName] = useState()
+    const [atvName, set_atvName] = useState()
+    const [cargo, setCargo] = useState()
+    const [data_inicio, set_data_inicio] = useState()
+    const [prazo, setPrazo] = useState()
+    const [importancia , setImportancia] = useState()
+
+    //o status é definido conforme a data_inicio e o prazo -> fazer função 
+    const [status , setStatus] = useState()
+    
